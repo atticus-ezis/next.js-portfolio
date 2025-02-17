743bf6b5951aaa5bc06eef2aeea18e50046dda74
Atticus Ezis <atticus.ezis@gmail.com>
2025-02-16 18:23:01 -0700

    mobile responsive about section

--- components/About.tsx
+++ components/About.tsx
@@ -2,8 +2,52 @@ import React from 'react';
 
 export const About = () => {
   return (
-    <div className="mt-[12rem] grid grid-cols-2 gap-[2rem]">
-      <div className="">
+    <div className="container mx-auto p-8 h-[80%]">
+      <h1 className="text-5xl center">About</h1>
+
+      <div className="flex flex-col md:flex-row">
+        <div className="w-full md:w-2/3 flex flex-col justify-between">
+          <div>
+            <h1>Web Design</h1>
+            <p>
+              I'm a full-stack web developer, who's worked with a wide range of frameworks, especially Django, which
+              I've used to contributed to the redesign of{' '}
+              <a href="https://americanliterature.com/">AmericanLiterature.com</a>, a popular platform that attracts
+              over 400,000 visitors each month.
+            </p>
+            <div className="bg-white rounded-md min-w-10 min-h-10 w-fit p-4">
+              <table className=""></table>
+            </div>
+          </div>
+          <div>
+            <h1>Data Analytics</h1>
+            <p>
+              I've used linear regressions to predict crypto pricing with Python and increased my investment return by
+              24%
+            </p>
+          </div>
+          <div>
+            <h1>LMM Development</h1>
+            <p>
+              I can utilize Large Language Models and develop RAG applications to build custom AIs like the one on this
+              site.
+            </p>
+          </div>
+        </div>
+
+        <div className="w-full md:w-1/3 flex flex-col order-first md:order-none">
+          <h1>Column right</h1>
+          <div className="center">
+            <img className="w-64 h-64 md:w-96 md:h-96 rounded-full object-cover mt-5" src="/profile.JPG"></img>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+{
+  /* <div className="">
         <div className="animated fadeInLeft">
           <h2>About me</h2>
         </div>
@@ -67,7 +111,5 @@ export const About = () => {
       </div>
       <div className="flex justify-center p-8">
         <img className="rounded-full object-cover" src="/profile.JPG"></img>
-      </div>
-    </div>
-  );
-};
+      </div> */
+}
