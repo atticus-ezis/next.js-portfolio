a5451d51123af9936398b48b156b0eba9db7ebd2
Atticus Ezis <atticus.ezis@gmail.com>
2025-02-17 13:45:20 -0700

    footer background added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,22 +1,16 @@
-import React from "react";
-import Image from "next/image";
+import React from 'react';
+import Image from 'next/image';
 
 export const Footer = () => {
   return (
-    <footer className="fixed bottom-0 w-full p-4 flex gap-6 flex-wrap items-center justify-center z-10 ">
+    <footer className="fixed bottom-0 w-full p-4 flex gap-6 flex-wrap items-center justify-center z-10 bg-[#0a0a0a]">
       <a
         className="flex items-center gap-2 hover:underline hover:underline-offset-4"
         href="https://github.com/atticus-ezis"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <Image
-          aria-hidden
-          src="/github.svg"
-          alt="File icon"
-          width={16}
-          height={16}
-        />
+        <Image aria-hidden src="/github.svg" alt="File icon" width={16} height={16} />
         GitHub
       </a>
       <a
@@ -25,13 +19,7 @@ export const Footer = () => {
         target="_blank"
         rel="noopener noreferrer"
       >
-        <Image
-          aria-hidden
-          src="/linkedin.png"
-          alt="Window icon"
-          width={16}
-          height={16}
-        />
+        <Image aria-hidden src="/linkedin.png" alt="Window icon" width={16} height={16} />
         Linkedin
       </a>
       <a
@@ -40,13 +28,7 @@ export const Footer = () => {
         target="_blank"
         rel="noopener noreferrer"
       >
-        <Image
-          aria-hidden
-          src="/gmail.svg"
-          alt="Globe icon"
-          width={16}
-          height={16}
-        />
+        <Image aria-hidden src="/gmail.svg" alt="Globe icon" width={16} height={16} />
         Email
       </a>
     </footer>
