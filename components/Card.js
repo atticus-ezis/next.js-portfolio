95c9f6a39a91b35afb7e37b7e874e36a842d2d48
Atticus Ezis <atticus.ezis@gmail.com>
2025-02-19 20:49:29 -0700

    uniform card layout

--- components/Card.js
+++ components/Card.js
@@ -3,33 +3,32 @@ import React from 'react';
 function Card({ image, title, description, link, github, skill }) {
   return (
     <>
-      <div className="w-[300px] h-[300px] bg-green-400 rounded-lg p-1">
-        {/* title */}
-        <h1 className="text-center text-xl font-bold">{title}</h1>
+      <div className="w-[320px] h-fit bg-white rounded-xl p-4 flex flex-col items-center">
+        <h1 className="text-center text-2xl font-bold text-gray-600 mb-3 w-full truncate overflow-hidden text-ellipsis whitespace-nowrap">
+          {title}
+        </h1>
         {/* card */}
-        <div className="p-4 rounded-lg shadow-lg transition-transform duration-300 hover:scale-105 hover:shadow-xl group">
+        <div className="relative w-full rounded-lg overflow-hidden shadow-lg transition-transform duration-300 hover:scale-105 hover:shadow-xl group">
           <a className="block" href={`${link}`} target="_blank">
-            <div className="relative">
-              {/* image */}
-              <div>
-                <img className="w-64 h-40 rounded-lg object-cover mx-auto" src={`/${image}`} alt={title}></img>
-              </div>
-              {/* description overlay */}
-              <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 transition-opacity duration-600 md:group-hover:opacity-100">
-                {description}
-              </div>
+            {/* image */}
+
+            <img className="w-full h-48 object-cover rounded-lg mx-auto" src={`/${image}`} alt={title}></img>
+
+            {/* description overlay */}
+            <div className="absolute inset-0 bg-black bg-opacity-60 flex items-center justify-center p-4 text-center opacity-0 transition-opacity duration-600 md:group-hover:opacity-100">
+              {description}
             </div>
           </a>
-          {/* skills */}
-          <div className="flex flex-col md:flex-row w-64 md:w-full items-center justify-center gap-2 bg-blue-700 rounded-lg mt-3 p-3 text-gray-200 text-sm text-center font-medium mx-auto">
-            <div className="w-fit h-fit">
-              <div className="">{skill}</div>
-            </div>
-            <div className="flex-shrink-0 transition-transform hover:scale-105 hover:cursor-pointer">
-              <a href={`${github}`}>
-                <img src="/github.svg" className="w-12 h-12 flex-shrink-0" alt=" GitHub Log" />
-              </a>
-            </div>
+        </div>
+        {/* skills */}
+        <div className="flex items-center justify-between w-full h-[75px] mt-4 px-4 -blue-700 rounded-lg p-3 text-gray-200 bg-blue-700 text-sm text-center font-medium mx-auto ">
+          <div className="">
+            <div className="">{skill}</div>
+          </div>
+          <div className="flex-shrink-0 transition-transform hover:scale-105 hover:cursor-pointer">
+            <a href={`${github}`}>
+              <img src="/github.svg" className="w-12 h-12 flex-shrink-0" alt=" GitHub Log" />
+            </a>
           </div>
         </div>
       </div>
