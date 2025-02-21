730c812c03d8789eb6d016150d37570be2532800
Atticus Ezis <atticus.ezis@gmail.com>
2025-02-20 22:29:28 -0700

    removed scrollbar from hero and added animaiton

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,8 +4,37 @@ import Link from 'next/link';
 import { scrollToSection } from '@/utils/scrollToSection';
 
 export const Header = () => {
+  const [hideHeader, setHideHeader] = useState(false);
+  useEffect(() => {
+    const scrollContainer = document.querySelector('.dynamic-vh');
+    const heroSection = document.getElementById('hero');
+    if (!heroSection || !scrollContainer) return;
+
+    const checkHero = () => {
+      const bound = heroSection.getBoundingClientRect();
+      if (bound.bottom <= 0) {
+        setHideHeader(false);
+      } else {
+        setHideHeader(true);
+      }
+
+      console.log('hero bottom is: ', bound.bottom);
+    };
+
+    checkHero();
+    scrollContainer.addEventListener('scroll', checkHero);
+
+    return () => {
+      scrollContainer.removeEventListener('scroll', checkHero);
+    };
+  }, []);
+
   return (
-    <header className="fixed top-0 w-full p-4 flex gap-6 justify-center mr-0 z-10">
+    <header
+      className={`fixed top-0 w-full p-4 flex gap-6 justify-center mr-0 z-10 transition-opacity duration-400 ${
+        hideHeader ? 'opacity-0 pointer-events-none' : 'opacity-100'
+      }`}
+    >
       <nav className="bg-[#21212150] backdrop-blur-[6px] justify-center fixed top-0 w-full  shadow-md p-4 flex gap-4">
         <NavLink section="ai" name="ai" />
         <NavLink section="about" name="about" />
