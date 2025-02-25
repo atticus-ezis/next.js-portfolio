dfa14b51acdad65180a7f308d67f904838a55d7c
Atticus Ezis <atticus.ezis@gmail.com>
2025-02-25 16:56:44 -0700

    removed background and positioned image above

--- components/About.tsx
+++ components/About.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 
 export const About = () => {
   return (
-    <div className="relative top-14 flex h-[85vh] flex-col items-center justify-start">
-      <div className="no-scrollbar h-full overflow-y-auto rounded-lg bg-gray-100 px-6">
-        <h1 className="p-4 text-center text-5xl font-bold text-gray-700">About</h1>
+    <div className="md:px-30 flex flex-col items-center px-10 py-[55px]">
+      <div className="no-scrollbar h-fit rounded-lg">
+        <h1 className="p-4 text-center text-5xl font-bold">About</h1>
         <div className="flex flex-col gap-8 md:flex-row">
           {/* Left Content Section */}
-          <div className="flex flex-col justify-between gap-4 md:w-2/3">
+          <div className="flex flex-col justify-center gap-4 md:w-2/3">
+            {/* Right Profile Section */}
+            <div className="order-first flex flex-col items-center md:order-none">
+              <img
+                className="mt-5 h-64 w-64 rounded-full object-cover shadow-md md:h-80 md:w-80"
+                src="/profile.jpeg"
+                alt="Profile"
+              />
+            </div>
             {/* Web Design Section */}
             <div className="rounded-lg bg-white p-6 shadow-md">
               <h2 className="mb-2 text-2xl font-semibold text-gray-400">
@@ -63,15 +71,6 @@ export const About = () => {
                 <img src="/tensor.png" className="h-8" alt="TensorFlow" />
               </div>
             </div>
-          </div>
-
-          {/* Right Profile Section */}
-          <div className="order-first flex flex-col items-center md:order-none">
-            <img
-              className="mt-5 h-64 w-64 rounded-full object-cover shadow-md md:h-80 md:w-80"
-              src="/profile.jpeg"
-              alt="Profile"
-            />
           </div>
         </div>
       </div>
